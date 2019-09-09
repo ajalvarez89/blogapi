2bd9455acd8c2a56733d65867a092b4be2bb580f
Alvaro Alvarez <alvaro.alvarez@youse.com.co>
2019-09-09 15:05:08 -0500

    adding unit test

--- spec/requests/health_spec.rb
+++ spec/requests/health_spec.rb
@@ -3,18 +3,18 @@ require 'rails_helper'
 RSpec.describe 'Health endpoint', type: :request do
 
   describe 'GET /health' do 
-    before {get '/healt'}
+    before {get '/health'}
 
     it 'should return OK' do 
-      payload = JSON.parse(resonse.body)
+      payload = JSON.parse(response.body)
       expect(payload).not_to be_empty
       expect(payload['api']).to eq('OK')
     end
 
     it 'should return status code 200' do 
-      payload = JSON.parse(resonse.body)
+      payload = JSON.parse(response.body)
       expect(response).to have_http_status(200)
     end
-
   end
+
 end
