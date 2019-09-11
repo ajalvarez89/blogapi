20b2e2b980e5bc3ae9ffa36049b3a4b949155694
Alvaro Alvarez <alvaro.alvarez@youse.com.co>
2019-09-11 14:44:17 -0500

    adding spec for be_truthy examples

--- spec/requests/health_spec.rb
+++ spec/requests/health_spec.rb
@@ -16,4 +16,20 @@ RSpec.describe 'Health endpoint', type: :request do
     end
   end
 
+  describe "be_truthy matcher" do
+    it 'be_truthy matcher' do 
+      expect(true).to be_truthy 
+      expect(7).to be_truthy 
+      expect("foo").to be_truthy 
+      expect(nil).not_to be_truthy 
+      expect(false).not_to be_truthy 
+    
+      # deliberate failures
+      # expect(true).not_to be_truthy
+      # expect(7).not_to be_truthy
+      # expect("foo").not_to be_truthy
+      # expect(nil).to be_truthy
+      # expect(false).to be_truthy
+    end
+  end
 end
